bb3804978be6329a907b5586c76c4492b4b3ec41
Teogenes <teogenes.santos@gmail.com>
2020-04-01 17:12:27 -0300

    Criacao das classes Program, Client, Order, OrderItem, Product e OrderStatus

---
+++ src/entities/Order.java
@@ -0,0 +1,59 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import entities.enums.OrderStatus;
+
+public class Order {
+	private Date moment;
+	private OrderStatus status;
+	private Client client;
+	List<OrderItem> orderItem = new ArrayList<>();
+	
+	public Order() {
+		
+	}
+	public Order(Date moment, OrderStatus status, Client client) {
+		super();
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+	}
+	public Date getMoment() {
+		return moment;
+	}
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+	public OrderStatus getStatus() {
+		return status;
+	}
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+	public Client getClient() {
+		return client;
+	}
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	public List<OrderItem> getOrderItem() {
+		return orderItem;
+	}
+	
+	public void addItem(OrderItem item) {
+		orderItem.add(item);
+	}
+	public void removeItem(OrderItem item) {
+		orderItem.remove(item);
+	}
+	public double total() {
+		double sum = 0;
+		for (OrderItem order : orderItem) {
+			sum += order.subTotal();
+		}
+		return sum;
+	}	
+}
